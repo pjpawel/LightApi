3dc06864ea73e566ba0b61c7d780e039cd34a8f4
pjpawel <pawel.jan.podgorski@gmail.com>
2023-02-06 17:30:34 +0100

    Fix env loading

--- src/Env.php
+++ src/Env.php
@@ -11,6 +11,8 @@ class Env
     ];
 
     /**
+     * Get configuration from files in given directories
+     *
      * @param string $dir
      * @param string $defaultConfigFile
      * @return array
@@ -18,18 +20,25 @@ class Env
     public static function getConfigFromEnv(string $dir, string $defaultConfigFile = 'config.php'): array
     {
         $config = [];
+        $dir .= DIRECTORY_SEPARATOR;
         $files = scandir($dir);
         if (in_array($defaultConfigFile, $files)) {
-            $config = self::loadConfigFile($defaultConfigFile);
+            $config = self::loadConfigFile($dir . $defaultConfigFile);
         }
         foreach (self::ENV_FILES as $file) {
             if (in_array($file, $files)) {
-                $config = array_merge_recursive(self::loadConfigFile($file), $config);
+                $config = array_merge_recursive(self::loadConfigFile($dir . $file), $config);
             }
         }
         return $config;
     }
 
+    /**
+     * Method to get configuration array form a file
+     *
+     * @param string $filePath
+     * @return array
+     */
     public static function loadConfigFile(string $filePath): array
     {
         return require $filePath;
