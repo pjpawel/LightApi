ca7bf22072c954dc9e41008503f45a90e675307a
pjpawel <pawel.jan.podgorski@gmail.com>
2023-02-06 17:30:47 +0100

    Add loaded flag

--- src/Command/CommandLoader.php
+++ src/Command/CommandLoader.php
@@ -12,6 +12,7 @@ class CommandLoader
      * @var array<string,string>[]
      */
     public array $command = [];
+    public bool $loaded = false;
 
     /**
      * @param string[] $commandPaths
@@ -27,6 +28,7 @@ class CommandLoader
             $command = '';
             //Here load command names and paths
         }
+        $this->loaded = true;
     }
 
     public function getCommand(string $commandName): Command
