18fed5eaa1492d8468395084325ffd98676914ca
pjpawel <pawel.jan.podgorski@gmail.com>
2023-02-16 23:52:49 +0100

    Add simple logger to composer

---
+++ src/Components/Logger/SimpleLogger/Level.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace pjpawel\LightApi\Components\Logger\SimpleLogger;
+
+enum Level: int
+{
+
+    case Debug = 100;
+
+    case Info = 200;
+
+    case Notice = 250;
+
+    case Warning = 300;
+
+    case Error = 400;
+
+    case Critical = 500;
+
+    case Alert = 550;
+
+    case Emergency = 600;
+
+    /**
+     * @param Level $level Minimal log level
+     * @return bool
+     */
+    public function isToLog(Level $level): bool
+    {
+        return $this->value >= $level->value;
+    }
+
+    /*public static function fromName(string $name): self
+    {
+        return match ($name) {
+            'debug', 'Debug', 'DEBUG' => self::Debug,
+            'info', 'Info', 'INFO' => self::Info,
+            'notice', 'Notice', 'NOTICE' => self::Notice,
+            'warning', 'Warning', 'WARNING' => self::Warning,
+            'error', 'Error', 'ERROR' => self::Error,
+            'critical', 'Critical', 'CRITICAL' => self::Critical,
+            'alert', 'Alert', 'ALERT' => self::Alert,
+            'emergency', 'Emergency', 'EMERGENCY' => self::Emergency,
+        };
+    }*/
+
+}
---
+++ src/Components/Logger/SimpleLogger/SimpleLogger.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace pjpawel\LightApi\Components\Logger\SimpleLogger;
+
+use DateTime;
+use DateTimeZone;
+use Exception;
+use Psr\Log\LoggerInterface;
+
+class SimpleLogger implements LoggerInterface
+{
+
+    private const SEPARATOR = ' ';
+
+    private Level $logLevel;
+    private string $logFilePath;
+    private DateTimeZone $timeZone;
+
+    public function __construct(string $logFilePath, Level $logLevel)
+    {
+        if (!is_file($logFilePath)) {
+            $this->createLogFile($logFilePath);
+        }
+        $this->logFilePath = realpath($logFilePath);
+        $this->logLevel = $logLevel;
+        $this->timeZone = new DateTimeZone(date_default_timezone_get());
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function createLogFile(string $filePath): void
+    {
+        if (!file_put_contents($filePath, '')) {
+            throw new Exception('Cannot create log file');
+        }
+        chmod($filePath, 0777);
+    }
+
+    private function addRecord(Level $level, string $message, array $context): void
+    {
+        if ($level->isToLog($this->logLevel)) {
+            $message = (new DateTime('now', $this->timeZone))->format(DATE_RFC3339_EXTENDED)
+                . self::SEPARATOR . $level->name . self::SEPARATOR . $message;
+            if (!empty($context)) {
+                $message .= self::SEPARATOR . var_export($context, true);
+            }
+            file_put_contents($this->logFilePath, $message . PHP_EOL, FILE_APPEND);
+        }
+    }
+
+    public function emergency(\Stringable|string $message, array $context = []): void
+    {
+        $this->addRecord(Level::Emergency, $message, $context);
+    }
+
+    public function alert(\Stringable|string $message, array $context = []): void
+    {
+        $this->addRecord(Level::Alert, $message, $context);
+    }
+
+    public function critical(\Stringable|string $message, array $context = []): void
+    {
+        $this->addRecord(Level::Critical, $message, $context);
+    }
+
+    public function error(\Stringable|string $message, array $context = []): void
+    {
+        $this->addRecord(Level::Error, $message, $context);
+    }
+
+    public function warning(\Stringable|string $message, array $context = []): void
+    {
+        $this->addRecord(Level::Warning, $message, $context);
+    }
+
+    public function notice(\Stringable|string $message, array $context = []): void
+    {
+        $this->addRecord(Level::Notice, $message, $context);
+    }
+
+    public function info(\Stringable|string $message, array $context = []): void
+    {
+        $this->addRecord(Level::Info, $message, $context);
+    }
+
+    public function debug(\Stringable|string $message, array $context = []): void
+    {
+        $this->addRecord(Level::Emergency, $message, $context);
+    }
+
+    public function log($level, \Stringable|string $message, array $context = []): void
+    {
+        $this->addRecord(Level::Emergency, $message, $context);
+    }
+}
