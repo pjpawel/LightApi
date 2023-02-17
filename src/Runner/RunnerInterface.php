13d5b1b104d8b5bf79b3c02f77d5893c91e3c456
pjpawel <pawel.jan.podgorski@gmail.com>
2023-02-17 19:41:08 +0100

    Add description of run method

--- src/Runner/RunnerInterface.php
+++ src/Runner/RunnerInterface.php
@@ -4,6 +4,10 @@ namespace pjpawel\LightApi\Runner;
 
 interface RunnerInterface
 {
+    /**
+     * Run method is meant to be a main method to runners
+     * @return void
+     */
     public function run(): void;
 
 }
