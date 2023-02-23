411e85e28b6a239f7eda9f4be05b27a953bb17c8
pjpawel <pawel.jan.podgorski@gmail.com>
2023-02-23 18:08:02 +0100

    Correct console script

--- bin/console.php
+++ bin/console.php
@@ -6,6 +6,6 @@ use pjpawel\LightApi\Component\Runner\CliRunner;
 use pjpawel\LightApi\Env;
 use pjpawel\LightApi\Kernel;
 
-$runner = new CliRunner(new Kernel(Env::getConfigFromEnv(__DIR__ . '/../config/')), $_SERVER['argc'][1]);
+$runner = new CliRunner(new Kernel(Env::getConfigFromEnv(__DIR__ . '/../config/')), $_SERVER['argv'][1]);
 $runner->run();
 return $runner->result;
