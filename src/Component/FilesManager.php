1d3d6d9eb205afdd5229a6f013dfb10ed7efaba1
pjpawel <pawel.jan.podgorski@gmail.com>
2023-03-01 23:36:22 +0100

    Correct FilesManager isDir method

--- src/Component/FilesManager.php
+++ src/Component/FilesManager.php
@@ -5,19 +5,20 @@ namespace pjpawel\LightApi\Component;
 use FilesystemIterator;
 use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
+use SplFileInfo;
 
 class FilesManager
 {
 
     public function removeDirRecursive(string $dirPath): void
     {
+        /** @var SplFileInfo[] $files */
         $files = new RecursiveIteratorIterator(
             new RecursiveDirectoryIterator($dirPath, FilesystemIterator::SKIP_DOTS),
             RecursiveIteratorIterator::CHILD_FIRST
         );
-
         foreach ($files as $file) {
-            if ($file->isDir) {
+            if ($file->isDir()) {
                 $this->removeDirRecursive($file->getRealPath());
             } else {
                 unlink($file->getRealPath());
