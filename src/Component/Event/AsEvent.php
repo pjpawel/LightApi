88940e2f0f561191568d834f09aa7eacdecf86e6
pjpawel <pawel.jan.podgorski@gmail.com>
2023-03-10 00:23:50 +0100

    Introduce basic events

---
+++ src/Component/Event/AsEvent.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace pjpawel\LightApi\Component\Event;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_FUNCTION|Attribute::TARGET_METHOD)]
+class AsEvent
+{
+
+}
---
+++ src/Component/Event/EventHandler.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace pjpawel\LightApi\Component\Event;
+
+class EventHandler
+{
+
+    /**
+     * @var array<string
+     */
+    private array $events = [];
+
+    public function registerEvent(string $id): void
+    {
+
+    }
+
+    public function handle(string $eventId)
+    {
+
+    }
+}
